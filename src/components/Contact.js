eda2f7c3094d465b09163d269168df8ff01dc834
moneer824 <87421838+moneer824@users.noreply.github.com>
2022-01-14 00:50:38 +0530

    contact section done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./Contact.css";
 
 export default function Contact() {
-    return (
-        <div className='contact'>
-            <p>Contact Me</p>
+  return (
+    <div className="contact">
+      <div className="number">
+        <p>Contact Me</p>
+        <p>+91 7003407697</p>
+        <p>moneerashraf824@gmail.com</p>
+        <div className="cont">
+          <a target="_blank" rel='noreferrer' href="https://github.com/moneer824">
+            <i class="fab fa-github"></i>
+          </a>
+
+          <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/moneer-ashraf-7a4475218/">
+            <i class="fab fa-linkedin"></i>
+          </a>
         </div>
-    )
+      </div>
+      <div className="copyright">
+      No © copyright issues | Designed and developed by Moneer Ashraf 
+      </div>
+    </div>
+  );
 }

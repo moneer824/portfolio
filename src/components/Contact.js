a37ce39a98a57beec041cd571790c067f51a204a
moneer824 <87421838+moneer824@users.noreply.github.com>
2022-01-13 23:39:36 +0530

    done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import "./Contact.css";
 export default function Contact() {
     return (
         <div className='contact'>
-            <p>wow</p>
+            <p>Contact Me</p>
         </div>
     )
 }

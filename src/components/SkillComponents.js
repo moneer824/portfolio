3a79d531485bf540571b58450ae301996a3b1d10
moneer824 <87421838+moneer824@users.noreply.github.com>
2022-01-30 21:23:34 +0530

    done

--- src/components/SkillComponents.js
+++ src/components/SkillComponents.js
@@ -4,7 +4,9 @@ import"./SkillComponents.css"
 export default function SkillComponents(props) {
     return (
         <div className='skill-comp'>
-            <img src={props.img} alt="" />
+            {/* <img src={props.img} alt="" /> */}
+            <i className={props.icon}></i>
+            <p>{props.name}</p>
         </div>
     )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import logo1 from './skill-logo/html.png'
-import logo2 from './skill-logo/css.png'
-import logo3 from './skill-logo/js.png'
-import logo4 from './skill-logo/bootstrap.png'
-import logo5 from './skill-logo/mongodb.png'
-import logo6 from './skill-logo/react.png'
-import logo7 from './skill-logo/react-js.png'
-import logo8 from './skill-logo/vs.png'
+
 import"./Skills.css"
 import SkillComponents from './SkillComponents'
 
@@ -16,14 +9,14 @@ export default function Skills() {
             <p>Skills and Tools</p>
             <div className="tools">
 
-           <SkillComponents img={logo1}/>
-           <SkillComponents img={logo2}/>
-           <SkillComponents img={logo3}/>
-           <SkillComponents img={logo4}/>
-           <SkillComponents img={logo5}/>
-           <SkillComponents img={logo6}/>
-           <SkillComponents img={logo7}/>
-           <SkillComponents img={logo8}/>
+           <SkillComponents name ="HTML" icon="fab fa-html5"/>
+           <SkillComponents name ="CSS" icon="fab fa-css3-alt"/>
+           <SkillComponents name ="js" icon="fab fa-js"/>
+           <SkillComponents name ="BOOTSTRAP" icon="fab fa-bootstrap"/>
+           <SkillComponents name ="MONGODb" icon="fab fa-twitter"/>
+           <SkillComponents  name ="REACT" icon="fab fa-react"/>
+           <SkillComponents name ="NODE js" icon="fab fa-node-js"/>
+           <SkillComponents name ="VS CODE" icon="fab fa-twitter"/>
             </div>
         </div>
     )

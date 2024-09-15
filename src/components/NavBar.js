e4d9a9ac27e1dc2c56e5d43d57df5e6b0ebda5c0
Moneer Ashraf <moneerashraf824@gmail.com>
2024-09-15 08:20:20 +0530

    master

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,11 +14,11 @@ export default function NavBar() {
            <i className="fas fa-bars"></i>
            </label>
            <ul>
-               <li onClick={uncheck}> <i className="far fa-address-card"></i> <a href="#HOME"> Home </a></li>
-               <li onClick={uncheck}> <i className="far fa-address-card"></i> <a href="#ABOUT"> About </a></li>
-               <li onClick={uncheck}> <i className="fas fa-sitemap"></i> <a href="#SKILLS"> Skills </a></li>
-               <li onClick={uncheck}> <i className="fas fa-laptop"></i> <a href="#recent-work"> Projects </a></li>
-               <li onClick={uncheck}> <i className="far fa-file-alt"></i><a target="_blank" rel='noreferrer' href="https://drive.google.com/file/d/1PCcrnGgonMmAZSzq9f_Ltg57b03EpKtW/view?usp=sharing"> Resume </a> </li>
+               <li onClick={uncheck}>  <a href="#HOME"><i className="far fa-address-card"></i> Home </a></li>
+               <li onClick={uncheck}> <a href="#ABOUT"> <i className="far fa-address-card"></i>  About </a></li>
+               <li onClick={uncheck}> <a href="#SKILLS"><i className="fas fa-sitemap"></i>  Skills </a></li>
+               <li onClick={uncheck}>  <a href="#recent-work"><i className="fas fa-laptop"></i> Projects </a></li>
+               <li onClick={uncheck}> <a target="_blank" rel='noreferrer' href="https://drive.google.com/file/d/1PCcrnGgonMmAZSzq9f_Ltg57b03EpKtW/view?usp=sharing"><i className="far fa-file-alt"></i> Resume </a> </li>
            </ul>
         </nav>
     )
